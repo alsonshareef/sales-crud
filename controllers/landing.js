62675c2c0feed1c8536650aba75a86c0861fc5ff
Alson Shareef <alson.shareef@gmail.com>
2020-03-06 21:32:20 +0800

    Renamed callback methods to names which represent their CRUD purpose more clearly.

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,13 +5,13 @@ exports.get_landing = (req, res, next) => {
   res.render('landing', { title: 'Express' });
 };
 
-exports.show_leads = (req, res, next) => {
+exports.get_leads = (req, res, next) => {
   return models.Lead.findAll().then(leads => {
     res.render('leads', { title: 'Leads', leads: leads });
   });
 };
 
-exports.show_individualLead = (req, res, next) => {
+exports.get_individualLead = (req, res, next) => {
   return models.Lead.findOne({
     where: {
       id: req.params.lead_id
@@ -21,8 +21,12 @@ exports.show_individualLead = (req, res, next) => {
   });
 };
 
+exports.get_updateLead = (req, res, next) => {};
+
+exports.update_lead = (req, res, next) => {};
+
 /* POST callbacks */
-exports.submit_lead = (req, res, next) => {
+exports.post_lead = (req, res, next) => {
   return models.Lead.create({
     email: req.body.lead_email
   }).then(lead => {
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ let express = require('express');
 let router = express.Router();
 let landing = require('../controllers/landing');
 
-/* GET home page. */
+/* GET home landing page. */
 router.get('/', landing.get_landing);
 
-/* GET show leads */
-router.get('/leads', landing.show_leads);
+/* GET list of leads and individual leads */
+router.get('/leads', landing.get_leads);
+router.get('/lead/:lead_id', landing.get_individualLead);
 
-/* GET show individual lead */
-router.get('/lead/:lead_id', landing.show_individualLead);
+/* GET and POST for update lead page */
+router.get('/lead/:lead_id/edit', landing.get_updateLead);
+router.post('/lead/:lead_id/edit', landing.update_lead);
 
 /* POST submit lead */
-router.post('/', landing.submit_lead);
+router.post('/', landing.post_lead);
 
 module.exports = router;
