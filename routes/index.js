d2858b943cb4bb629a342cf9de491465b9ef7a7f
Alson Shareef <alson.shareef@gmail.com>
2020-03-06 22:03:16 +0800

    Reorganised into CRUD order

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ let landing = require('../controllers/landing');
 /* GET home landing page. */
 router.get('/', landing.get_landing);
 
+/* POST submit lead */
+router.post('/', landing.post_lead);
+
 /* GET list of leads and individual leads */
 router.get('/leads', landing.get_leads);
 router.get('/lead/:lead_id', landing.get_individualLead);
@@ -12,8 +15,5 @@ router.get('/lead/:lead_id', landing.get_individualLead);
 /* GET and POST for update lead page */
 router.get('/lead/:lead_id/edit', landing.get_updateLead);
 router.post('/lead/:lead_id/edit', landing.update_lead);
-
-/* POST submit lead */
-router.post('/', landing.post_lead);
 
 module.exports = router;
