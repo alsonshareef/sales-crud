c50809fde4bf6755b32029c88a4f782a33667571
Alson Shareef <alson.shareef@gmail.com>
2020-03-08 16:18:14 +0800

    update comments

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ let landingControllers = require('../controllers/landing');
 /* GET home landing page. */
 router.get('/', landingControllers.get_landing);
 
-/* POST submit lead */
+/* (CREATE) POST submit lead */
 router.post('/', landingControllers.post_lead);
 
-/* GET list of leads and individual leads */
+/* (READ) GET list of leads and individual leads */
 router.get('/leads', landingControllers.get_leads);
 router.get('/lead/:lead_id', landingControllers.get_individualLead);
 
-/* GET and POST for update lead page */
+/* (UPDATE) GET and POST for update lead page */
 router.get('/lead/:lead_id/edit', landingControllers.get_updateLead);
 router.post('/lead/:lead_id/edit', landingControllers.update_lead);
 
-/* POST for delete lead */
+/* (DELETE) POST for delete lead */
 router.post('/lead/:lead_id/delete', landingControllers.delete_lead);
 
 module.exports = router;
