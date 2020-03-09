c37702a3b510d742e23941aad30f8c9cf20c2ce3
Alson Shareef <alson.shareef@gmail.com>
2020-03-09 23:33:11 +0800

    signup, login and logout routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,17 @@
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const myPassport = require('../passport_setup')(passport);
+const flash = require('connect-flash');
+
 const models = require('../models');
 
+/* HELPER FUNCTIONS */
+
+const generateHashPassword = password => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+/* CALLBACKS */
 exports.show_login = (req, res, next) => {
   res.render('user/login', { formData: {}, errors: {} });
 };
@@ -8,6 +20,30 @@ exports.show_signup = (req, res, next) => {
   res.render('user/signup', { formData: {}, errors: {} });
 };
 
-exports.signup = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  const newUser = models.User.build({
+    email: req.body.email,
+    password: generateHashPassword(req.body.password)
+  });
+  return newUser.save().then(result => {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/signup',
+      failureFlash: true
+    })(req, res, next);
+  });
+};
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
+};
+
+exports.logout = (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+};
